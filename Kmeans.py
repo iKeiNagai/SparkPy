aae3017f00872c107e320288c171257e154ad073
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-02 15:44:58 -0400

    trains model and display cluster centers

---
+++ Kmeans.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.clustering import KMeans
+
+#Creates Session
+spark = SparkSession.builder \
+    .appName("KMeansModel") \
+    .getOrCreate()
+
+dataset = spark.read.format("libsvm").load("kmeans_input.txt") 
+
+#Model Training
+#Kmeans model
+kmeans = KMeans().setK(2).setSeed(1) #kmeans to 2 clusters,1 random initialization
+model = kmeans.fit(dataset) #performs clustering
+
+#model testing
+#apply trained model to dataset
+predictions = model.transform(dataset)
+
+#Get cluster center (vector form)
+centers = model.clusterCenters()
+
+#Displays cluster centers
+print("Cluster Centers: ")
+for center in centers:
+    print(center)
