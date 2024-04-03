24cbaa2a465a025eaf68af08b8b38e77c3119496
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-03 13:28:32 -0400

    loaded and plotted centroids from file

--- graphh.py
+++ graphh.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 #Load data from file
 array = np.loadtxt('features.txt')
 list = np.loadtxt('clusternum.txt')
+centers = np.loadtxt('centers.txt')
+
 #Extract data from file
 x = array[:, 0]
 y = array[:, 1]
@@ -17,9 +19,16 @@ y_cluster1 = y[list == 0]
 plt.scatter(x_cluster1, y_cluster1, color='red', marker='x', label='Cluster 1')
 
 #cluster 2 color blue and circle mark
-x_cluster1 = x[list == 1]
-y_cluster1 = y[list == 1]
+x_cluster2 = x[list == 1]
+y_cluster2 = y[list == 1]
 plt.scatter(x_cluster1, y_cluster1, color='blue', marker='o', label='Cluster 2')
+
+#extrated centers from file
+x_centroid = centers[0]
+y_centroid = centers[1]
+
+#Plot centers as red and triangle in graph
+plt.scatter(x_centroid,y_centroid, c='red', marker='^')
 
 #show graph
 plt.show()
