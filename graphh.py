5ce9d4991b3658337a74cf79d508744d195e29a1
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-03 13:01:05 -0400

    created custom color,mark and label for cluster 1 and 2

--- graphh.py
+++ graphh.py
@@ -3,11 +3,23 @@ import matplotlib.pyplot as plt
 
 #Load data from file
 array = np.loadtxt('features.txt')
-
+list = np.loadtxt('clusternum.txt')
 #Extract data from file
 x = array[:, 0]
 y = array[:, 1]
 
-# Plot the data as a scatter plot
-plt.scatter(x, y)
+#Plot data as a scatter plot
+plt.scatter(x, y, c=list)
+
+#cluster 1 color red and cross mark
+x_cluster1 = x[list == 0]
+y_cluster1 = y[list == 0]
+plt.scatter(x_cluster1, y_cluster1, color='red', marker='x', label='Cluster 1')
+
+#cluster 2 color blue and circle mark
+x_cluster1 = x[list == 1]
+y_cluster1 = y[list == 1]
+plt.scatter(x_cluster1, y_cluster1, color='blue', marker='o', label='Cluster 2')
+
+#show graph
 plt.show()
