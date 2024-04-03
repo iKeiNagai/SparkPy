66dd1ed346871987dd931a51c39e1eeb07b5beee
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-03 17:32:02 -0400

    changed var name for understanding

--- graphh.py
+++ graphh.py
@@ -3,32 +3,31 @@ import matplotlib.pyplot as plt
 
 #Load data from file
 array = np.loadtxt('features.txt')
-list = np.loadtxt('clusternum.txt')
+clusternum = np.loadtxt('clusternum.txt')
 centers = np.loadtxt('centers.txt')
 
 #Extract data from file
-x = array[:, 0]
-y = array[:, 1]
+x = array[: ,0]
+y = array[: ,1]
 
 #Plot data as a scatter plot
 plt.scatter(x, y)
 
 #cluster 1 color red and cross mark
-x_cluster1 = x[list == 0]
-y_cluster1 = y[list == 0]
+x_cluster1 = x[clusternum == 0]
+y_cluster1 = y[clusternum == 0]
 plt.scatter(x_cluster1, y_cluster1, color='red', marker='x', label='Cluster 1')
 
 #cluster 2 color blue and circle mark
-x_cluster2 = x[list == 1]
-y_cluster2 = y[list == 1]
+x_cluster2 = x[clusternum == 1]
+y_cluster2 = y[clusternum == 1]
 plt.scatter(x_cluster2, y_cluster2, color='blue', marker='o', label='Cluster 2')
 
 #extrated centers from file
-x_centroid = centers[0]
-y_centroid = centers[1]
+centroid_1 = centers[0]
+centroid_2 = centers[1]
 
 #Plot centers as red and triangle in graph
-plt.scatter(x_centroid,y_centroid, c='yellow', marker='^')
-
+plt.scatter(centroid_1,centroid_2 ,c='yellow', marker='^')
 #show graph
 plt.show()
