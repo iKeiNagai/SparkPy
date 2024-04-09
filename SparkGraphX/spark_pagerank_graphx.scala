559532a260cac66b3fd989ec5971c1d56a93c6b0
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-09 17:56:42 -0400

    scala program for pagerank

---
+++ SparkGraphX/spark_pagerank_graphx.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.graphx.{Graph, Edge}
+import org.apache.spark.SparkContext
+
+//spark context
+val conf = new SparkConf()
+  .setAppName("Pagerank")
+  .setMaster("local[*]") 
+val sc = new SparkContext(conf)
+
+//loads file
+val file = sc.textFile("02AdjacencyList.txt")
+
+//prints file
+file.collect().foreach(println)
