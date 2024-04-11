233ac1966de3539056851d71f9c8a679cdd1fa13
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-10 22:02:12 -0400

    feat: create edges and graph. display graph vertices&edges

--- SparkGraphX/spark_pagerank_graphx.scala
+++ SparkGraphX/spark_pagerank_graphx.scala
@@ -10,13 +10,24 @@ val sc = new SparkContext(conf)
 //loads file
 val file = sc.textFile("02AdjacencyList.txt")
 
-//creates array of string for each line
-val linesArray = file.map(line => line.split(" "))
+//creates array of string for each line, converts to long
+val linesArray = file.map(line => line.split(" ").map(.toLong))
 
 //gets first items of array, map vertices to 1
-val firstItems = linesArray.map(arr => arr(0)).map(vertice => (vertice,1))
+val firstItemstoVertice = linesArray.map(arr => arr(0)).map(vertice => (vertice,1))
 
-//get value
-val value = firstItems.collect()
+//map the edges 
+val edges = linesArray.flatMap { arr =>
+  val src = arr.head
+  arr.tail.map(dest => Edge(src, dest, 1))
+}
+
+//creates graph with vertices and edges 
+val graph = Graph(firstItemstoVertice, edges)
+
+//displays graph vertices and edges
+val getvertices = graph.vertices.collect()
+val getedges = graph.edges.collect()
+
 //prints file
 file.collect().foreach(println)
