fa0a2deb57cfaac8072c38a0dbacace21ee7d809
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-12 13:57:10 -0400

    feat: calculate pagerank

--- SparkGraphX/spark_pagerank_graphx.scala
+++ SparkGraphX/spark_pagerank_graphx.scala
@@ -17,17 +17,34 @@ val linesArray = file.map(line => line.split(" ").map(.toLong))
 val firstItemstoVertice = linesArray.map(arr => arr(0)).map(vertice => (vertice,1))
 
 //map the edges 
-val edges = linesArray.flatMap { arr =>
+val create_edges = linesArray.flatMap { arr =>
   val src = arr.head
   arr.tail.map(dest => Edge(src, dest, 1))
 }
 
 //creates graph with vertices and edges 
-val graph = Graph(firstItemstoVertice, edges)
+val graph = Graph(firstItemstoVertice, create_edges)
 
 //displays graph vertices and edges
 val getvertices = graph.vertices.collect()
 val getedges = graph.edges.collect()
+
+//calculate pagerank
+val PageRankGraph1 = graph.staticPageRank(30, 0.15)
+
+//stores pagrerank with vertices.
+val PageRankVertices1 = PageRankGraph1.vertices
+
+//total pagerank sum 
+val dSum1 = PageRankVertices1.map(values => values._2).sum()
+
+//each pagerank is divided by pagerank sum
+val PageRankVertices11 = PageRankVertices1.mapValues(prValue=> prValue / dSum1)
+
+//displays pagerank
+val PageRankValues1 = PageRankVertices11.collect()
+println("PageRank:")
+println(PageRankValues1.mkString("\n"))
 
 //prints file
 file.collect().foreach(println)
